9eccd2dabb98e725c48a38eb6240a338e2553333
adinugroh0 <aditwist1@gmail.com>
2024-09-19 07:15:43 +0700

    jajal

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
+module.exports = nextConfig;
